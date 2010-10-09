7938864fc80bf94b0f2d557f9ff1537845ad6f2e
Peter West <peterjwest3@gmail.com>
2010-10-09 23:05:33 +0100

    added ability to parse start tags and their attributes

--- yawn.php
+++ yawn.php
@@ -1,16 +1,40 @@
 <?php
 class YawnNode {
+	var $name = "";
 	var $attrs = false;
 	var $children = array();
-	var $done = false;
+	var $singular = false;
+	var $parsed = false;
 	var $tail = "";
-		
-	function __construct($html) { $this->tail = $html; }
-	function unique($selector) { return preg_match("~#[^\s]*$~", $selector); }
+	var $lazy = true;
+
+	function __construct($html) {
+		$this->tail = $html; 
+		$match = $this->get("<([^\s<>]+)");
+		$this->name = $match[1];
+	}
 	
-	function find($selectors) { 
+	function unique($selector) { return strpos("#", $selector) !== false; }
+	
+	function get($regex) {
+		if (preg_match("~^\s*".$regex."~", $this->tail, $match)) {
+			$this->tail = substr($this->tail, strlen($match[0]));
+			return $match;
+		}
+	}
+	
+	function getFirst($string) {
+		$pos = strpos($this->tail, $string);
+		if ($pos !== false) {
+			$inner = substr($this->tail, 0, $pos - 1);
+			$this->tail = substr($this->tail, $pos + strlen($string));
+			return $inner;
+		}
+	}
+	
+	function find($selectors) {
 		$this->attrs();
-		$unique = $this->unique($selectors)
+		$unique = $this->unique($selectors);
 		$found = array();
 		$remaining = $this->matches($selectors);
 		if ($remaining === true) {
@@ -29,20 +53,44 @@ class YawnNode {
 		}
 		return $found;
 	}
-
+	
 	function matches($selectors) {
-		$selectors = preg_split("[\s]+",$selectors,1);
+		$selectors = preg_split("\s+",$selectors,1);
 		foreach (preg_split("~(?=[\.#@])~",$selectors[0]) as $selector) {
 			if (substr($selector,0,1) === "#")
 				if ($this->attr("id") !== substr($selector,1)) return false;
 			if (substr($selector,0,1) === ".")
-				if (!in_array(substr($selector,1), explode(" ", $this->attr("class")))) 
+				if (!in_array(substr($selector,1), explode(" ", $this->attr("class"))))
 					return false;
 			if (substr($selector,0,1) === "@")
-				if (!in_array(substr($selector,1), explode(" ", $elem->attr("stim:id")))) 
+				if (!in_array(substr($selector,1), explode(" ", $elem->attr("stim:id"))))
 					return false;
 		}
-		return isset($selectors[1]) $selectors[1] : true;
+		return isset($selectors[1]) ? $selectors[1] : true;
+	}
+	
+	function attr($name, $value = false) {
+		$this->attrs();
+		if (count(func_get_args()) > 1) {
+			if ($value === false) unset($this->attr[$name]);
+			else $this->attr[$name] = $value;
+			return $this;
+		}
+		return isset($this->attr[$name]) ? $this->attr[$name] : false;
+	}
+	
+	function parseAttrs() {
+		if ($this->attrs !== false) return;
+		while ($name = $this->get("\s+([^\s=<>]+)")) {
+			if ($this->get("=")) 
+				($value = $this->get('"([^"]*)"')) || ($value = $this->get("'([^']*)'")) || ($value = $this->get("([^\s</>]*)"));
+			$this->attrs[] = array($name[1], isset($value[1]) ? $value[1] : true);
+		}
+		if ($close = $this->get("(/?)>")) { if ($close[1]) { $this->singular = true; $this->parsed = true; } }
+		else throw new Exception("'".$this->name."' start tag not closed");
 	}
 
+	function parseComment() {
+		if ($this->get("<!--"))	if(!$this->getFirst("-->")) throw new Exception("HTML comment not closed");
+	}
 }
